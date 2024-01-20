928778fb036d6894b899b0e0c0d1b28425ab4fec
Josh Chang <joshchang04@gmail.com>
2024-01-19 23:35:15 -0500

    Add favicon to the renderer

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -18,6 +18,7 @@ export const renderer = jsxRenderer(
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="description" content="The scoreboard for the Yale CPSC 323 Bomb Lab problem set." />
+          <link rel="icon" href="/static/favicon.svg" type="image/svg+xml" />
         </head>
         <body lang="en" class="bg-slate-800 font-mono">
           {children}
