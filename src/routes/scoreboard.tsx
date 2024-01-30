cdc0e0611f036cc086e9c491c9128c6f1799ffae
Josh Chang <joshchang04@gmail.com>
2024-01-30 03:00:39 -0500

    add mobile styles

--- src/routes/scoreboard.tsx
+++ src/routes/scoreboard.tsx
@@ -2,6 +2,53 @@ import { drizzle } from "drizzle-orm/d1";
 import { C } from "..";
 import * as schema from "../schema";
 
+export function PodiumItem(props: {
+  place: number;
+  customClass: string;
+  bomb: schema.Bomb | undefined;
+  showNetIds: boolean;
+}) {
+  return (
+    <div
+      class={
+        "grid grid-cols-[60px_2fr] md:grid-cols-[0.8fr_2fr] lg:grid-cols-[1fr_2fr] overflow-hidden rounded bg-slate-900 text-center " +
+        props.customClass
+      }
+    >
+      <div class="flex items-center justify-center bg-rose-500/80 text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-slate-50 px-5 py-2 flex-grow">
+        {props.place}
+      </div>
+      <div class="flex items-center justify-between md:justify-center text-lg sm:text-xl lg:text-2xl text-slate-100 md:flex-col gap-4 sm:gap-2 lg:gap-3 px-6 py-2 sm:py-3 md:py-4 text-center">
+        <span>{props.bomb ? "bomb" + props.bomb.id : "--"}</span>
+        {props.bomb &&
+          (props.showNetIds ? (
+            <span class="text-sm sm:text-base text-slate-400 flex md:flex-col gap-2">
+              <span>{props.bomb.netId}</span>
+              <span>({props.bomb.score} points)</span>
+            </span>
+          ) : (
+            <span class="text-sm sm:text-base text-slate-400">
+              {props.bomb.score} points
+            </span>
+          ))}
+      </div>
+    </div>
+  );
+}
+
+function getPodiumBomb(bombs: schema.Bomb[], index: number) {
+  let bomb = bombs[index] as schema.Bomb | undefined;
+  if (
+    bombs[index] &&
+    bombs[index].score === 0 &&
+    bombs[index].explosions === 0 &&
+    bombs[index].phase === 0
+  ) {
+    bomb = undefined;
+  }
+  return bomb;
+}
+
 // main scoreboard page with the top 3 bombs and a table of all bombs
 export async function scoreboard(c: C) {
   // get the bombs from the database
@@ -11,144 +58,94 @@ export async function scoreboard(c: C) {
   });
 
   // get the top 3 bombs
-  let first = bombs[0] as schema.Bomb | undefined;
-  if (
-    first &&
-    first.score === 0 &&
-    first.explosions === 0 &&
-    first.phase === 0
-  ) {
-    first = undefined;
-  }
-  let second = bombs[1] as schema.Bomb | undefined;
-  if (
-    second &&
-    second.score === 0 &&
-    second.explosions === 0 &&
-    second.phase === 0
-  ) {
-    second = undefined;
-  }
-  let third = bombs[2] as schema.Bomb | undefined;
-  if (
-    third &&
-    third.score === 0 &&
-    third.explosions === 0 &&
-    third.phase === 0
-  ) {
-    third = undefined;
-  }
+  const first = getPodiumBomb(bombs, 0);
+  const second = getPodiumBomb(bombs, 1);
+  const third = getPodiumBomb(bombs, 2);
 
   // get the showNetIds query param from the url if it exists
   const { searchParams } = new URL(c.req.url);
   const showNetIds = searchParams.get("netid") === "true";
 
+  const cols = showNetIds
+    ? "grid md:grid-cols-[4rem_6rem_6rem_7rem_10rem_12rem] lg:grid-cols-[6rem_9rem_10rem_9rem_12rem_15rem] items-center"
+    : "grid grid-cols-[2rem_3.5rem_4rem_6rem_7rem] sm:grid-cols-[3rem_5rem_6rem_8rem_9rem] md:grid-cols-[4rem_6rem_7rem_10rem_12rem] lg:grid-cols-[6rem_9rem_9rem_12rem_15rem] items-center";
+
   // render the scoreboard page UI
   return c.render(
-    <div class="min-h-screen flex flex-col">
+    <div class="absolute inset-0 flex flex-col">
       <div class="flex-grow">
         {/* header */}
-        <div class="flex items-center justify-between bg-slate-100 px-16 py-10">
+        <div class="flex flex-col sm:flex-row gap-5 sm:items-center sm:justify-between bg-slate-100 px-6 py-6 sm:px-9 md:px-12 md:py-8 lg:px-16 lg:py-10">
           <div class="grid gap-3">
-            <h2 class="flex items-center gap-3 text-lg font-medium">
+            <h2 class="flex items-center gap-3 sm:text-lg font-medium">
               <span class="text-slate-600">CPSC 323</span>
               <div class="h-5 w-0.5 rounded-full bg-slate-300"></div>
               <span class="text-rose-600">Bomb Lab</span>
             </h2>
-            <h1 class="font-mono text-3xl font-bold text-slate-900">
+            <h1 class="font-mono text-xl sm:text-2xl md:text-3xl font-bold text-slate-900">
               Scoreboard
             </h1>
           </div>
-          <div class="w-1/2 leading-normal text-slate-600">
+          <div class="text-sm md:text-base sm:w-1/2 leading-normal text-slate-600">
             This page contains the latest information that we have received from
             your bomb. If you have any questions, please contact the course
             staff on Ed Discussion or by email.
           </div>
         </div>
         {/* podium */}
-        <div class="grid grid-cols-3 items-end gap-12 p-16">
-          {/* second */}
-          <div class="grid h-44 grid-cols-[1fr_2fr] overflow-hidden rounded bg-slate-900">
-            <div class="flex items-center justify-center bg-rose-500/80 text-5xl font-bold text-slate-50">
-              2
-            </div>
-            <div class="flex items-center justify-center text-2xl text-slate-100 flex-col gap-3">
-              <span>{second ? "bomb" + second.id : "--"}</span>
-              {second &&
-                (showNetIds ? (
-                  <span class="text-base text-slate-400">
-                    {second.netId} ({second.score} points)
-                  </span>
-                ) : (
-                  <span class="text-base text-slate-400">
-                    {second.score} points
-                  </span>
-                ))}
-            </div>
-          </div>
+        <div class="grid md:grid-cols-3 items-end gap-3 sm:gap-5 md:gap-8 lg:gap-12 p-4 sm:p-8 md:p-12 lg:p-16 mt-4">
           {/* first */}
-          <div class="grid h-60 grid-cols-[1fr_2fr] overflow-hidden rounded bg-slate-900">
-            <div class="flex items-center justify-center bg-rose-500/80 text-5xl font-bold text-slate-50">
-              1
-            </div>
-            <div class="flex items-center justify-center text-2xl text-slate-100 flex-col gap-3">
-              <span>{first ? "bomb" + first.id : "--"}</span>
-              {first &&
-                (showNetIds ? (
-                  <span class="text-base text-slate-400">
-                    {first.netId} ({first.score} points)
-                  </span>
-                ) : (
-                  <span class="text-base text-slate-400">
-                    {first.score} points
-                  </span>
-                ))}
-            </div>
-          </div>
+          <PodiumItem
+            place={1}
+            customClass="md:h-60 md:order-2"
+            bomb={first}
+            showNetIds={showNetIds}
+          />
+          {/* second */}
+          <PodiumItem
+            place={2}
+            customClass="md:h-48 md:order-1"
+            bomb={second}
+            showNetIds={showNetIds}
+          />
           {/* third */}
-          <div class="grid h-32 grid-cols-[1fr_2fr] overflow-hidden rounded bg-slate-900">
-            <div class="flex items-center justify-center bg-rose-500/80 text-5xl font-bold text-slate-50">
-              3
-            </div>
-            <div class="flex items-center justify-center text-2xl text-slate-100 flex-col gap-3">
-              <span>{third ? "bomb" + third.id : "--"}</span>
-              {third &&
-                (showNetIds ? (
-                  <span class="text-base text-slate-400">
-                    {third.netId} ({third.score} points)
-                  </span>
-                ) : (
-                  <span class="text-base text-slate-400">
-                    {third.score} points
-                  </span>
-                ))}
-            </div>
-          </div>
+          <PodiumItem
+            place={3}
+            customClass="md:h-36 md:order-3"
+            bomb={third}
+            showNetIds={showNetIds}
+          />
         </div>
         {/* results */}
         {bombs.length > 0 ? (
-          <div class="grid gap-2 px-16 pb-16 pt-4">
-            <div class="flex items-center justify-between gap-3 px-1 text-lg text-slate-50">
-              <div
-                class={
-                  showNetIds
-                    ? "grid grid-cols-[6rem_9rem_10rem_9rem_12rem_16rem] items-center"
-                    : "grid grid-cols-[6rem_9rem_9rem_12rem_16rem] items-center"
-                }
-              >
-                <div class="text-2xl font-medium text-slate-600">#</div>
-                <div class="text-lg font-medium text-slate-600">Bomb</div>
+          <div class="grid gap-0.5 sm:gap-1 md:gap-1.5 lg:gap-2 px-4 sm:px-8 md:px-12 lg:px-16 pb-8 sm:pb-10 md:pb-12 lg:pb-16 pt-2 sm:pt-3 md:pt-4 overflow-x-scroll">
+            <div class="flex items-center justify-between gap-3 px-1 text-slate-50 p-2">
+              <div class={cols}>
+                <div class="text-base sm:text-lg md:text-xl lg:text-2xl font-medium text-slate-600">
+                  #
+                </div>
+                <div class="text-xs sm:text-sm md:text-base lg:text-lg font-medium text-slate-600">
+                  Bomb
+                </div>
                 {showNetIds && (
-                  <div class="text-lg font-medium text-slate-600">NetID</div>
+                  <div class="text-xs sm:text-sm md:text-base lg:text-lg font-medium text-slate-600">
+                    NetID
+                  </div>
                 )}
-                <div class="text-lg font-medium text-slate-600">Phase</div>
-                <div class="text-lg font-medium text-slate-600">Explosions</div>
-                <div class="text-lg font-medium text-slate-600">
+                <div class="text-xs sm:text-sm md:text-base lg:text-lg font-medium text-slate-600">
+                  Phase
+                </div>
+                <div class="text-xs sm:text-sm md:text-base lg:text-lg font-medium text-slate-600">
+                  Explosions
+                </div>
+                <div class="text-xs sm:text-sm md:text-base lg:text-lg font-medium text-slate-600">
                   Submission Time
                 </div>
               </div>
               <div class="flex items-center gap-4">
-                <div class="text-lg font-medium text-slate-600">Score</div>
+                <div class="text-xs sm:text-sm md:text-base lg:text-lg font-medium text-slate-600">
+                  Score
+                </div>
               </div>
             </div>
             <div class="h-px w-full rounded-lg bg-slate-700"></div>
@@ -156,16 +153,10 @@ export async function scoreboard(c: C) {
               <>
                 <div
                   id={"bomb" + bomb.id}
-                  class="flex items-center justify-between gap-3 px-1 py-1 text-lg text-slate-50"
+                  class="flex items-center justify-between gap-1.5 sm:gap-2 md:gap-2.5 lg:gap-3 px-1 py-1 text-xs sm:text-sm md:text-base lg:text-lg text-slate-50"
                 >
-                  <div
-                    class={
-                      showNetIds
-                        ? "grid grid-cols-[6rem_9rem_10rem_9rem_12rem_16rem] items-center"
-                        : "grid grid-cols-[6rem_9rem_9rem_12rem_16rem] items-center"
-                    }
-                  >
-                    <div class="text-3xl font-bold text-slate-500">
+                  <div class={cols}>
+                    <div class="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold text-slate-500">
                       {index + 1}
                     </div>
                     <div>bomb{bomb.id}</div>
@@ -188,7 +179,7 @@ export async function scoreboard(c: C) {
                         {bomb.explosions}
                       </div>
                     )}
-                    <div class="text-base text-slate-400">
+                    <div class="text-xs sm:text-sm lg:text-base text-slate-400">
                       {bomb.phase > 0 || bomb.explosions > 0 || bomb.score !== 0
                         ? bomb.time.toLocaleString("en-US", {
                             day: "numeric",
@@ -202,7 +193,7 @@ export async function scoreboard(c: C) {
                     </div>
                   </div>
                   <div class="flex items-center gap-4">
-                    <div class="text-xl font-bold text-rose-500">
+                    <div class="text-base sm:text-lg md:text-lg lg:text-xl font-bold text-rose-500">
                       {bomb.score}
                     </div>
                   </div>
@@ -218,7 +209,7 @@ export async function scoreboard(c: C) {
         )}
       </div>
       {/* footer */}
-      <div class="flex items-center justify-between gap-3 px-16 py-6 border-t border-t-slate-700 bg-slate-900/30 text-slate-400 text-sm">
+      <div class="flex flex-col lg:flex-row items-center justify-between gap-3 sm:gap-4 md:gap-5 px-8 lg:px-16 py-6 border-t border-t-slate-700 bg-slate-900/30 text-slate-400 text-xs md:text-sm">
         <div>
           Bomb Lab Server by{" "}
           <a
--- src/routes/submit.ts
+++ src/routes/submit.ts
@@ -5,35 +5,37 @@ import { eq } from "drizzle-orm";
 import { C } from "..";
 import { clearScoreboardCache } from "../utils";
 
+// This is the submit route. It is used to submit the results of a defusal or explosion. Called by the bomb executables.
 export async function submit(c: C) {
   const { searchParams } = new URL(c.req.url);
   const netId = searchParams.get("netId");
   const result = searchParams.get("result");
   const secret = searchParams.get("secret");
 
+  // validate the parameters
   if (!netId || !result || !secret) {
     throw new HTTPException(400, { message: "Missing parameters" });
   }
 
-  const db = drizzle(c.env.DB, { schema });
-
+  // the format of the result is "bombId:action:phase:response"
   const pieces = result.split(":");
-
   if (pieces.length < 4) {
     throw new HTTPException(400, { message: "Invalid result" });
   }
   if (pieces.length > 4) {
     pieces[3] = pieces.slice(3).join(":");
   }
-
   const [bombIdStr, action, phaseStr, response] = pieces;
   const bombId = parseInt(bombIdStr);
   const phase = parseInt(phaseStr);
-
+  
+  // get the bomb from the database
+  const db = drizzle(c.env.DB, { schema });
   const current = await db.query.bombs.findFirst({
     where: (bombs, { eq }) => eq(bombs.id, bombId),
   });
 
+  // validate the bomb
   if (!current) {
     throw new HTTPException(400, { message: "Invalid bombId" });
   }
@@ -44,16 +46,18 @@ export async function submit(c: C) {
     throw new HTTPException(400, { message: "Invalid secret" });
   }
 
+  // bomb claims to be defused at phase
   if (action === "defused") {
+
+    // validate the phase and response
     if (!response) {
       throw new HTTPException(400, { message: "Missing response" });
     }
-
-    console.log(current.phase, phase);
     if (phase !== current.phase + 1 || phase > 10) {
       throw new HTTPException(400, { message: "Invalid phase" });
     }
 
+    // update the bomb and insert the defusal log
     await db
       .update(schema.bombs)
       .set({
@@ -62,7 +66,6 @@ export async function submit(c: C) {
         score: phase * 10 + current.explosions * -0.5,
       })
       .where(eq(schema.bombs.id, bombId));
-
     await db.insert(schema.defusals).values({
       bombId,
       response,
@@ -72,7 +75,11 @@ export async function submit(c: C) {
     await clearScoreboardCache(c);
 
     return c.text("OK");
-  } else if (action === "exploded") {
+  } 
+  // bomb claims to have exploded
+  else if (action === "exploded") {
+
+    // update the bomb and insert the explosion log
     await db
       .update(schema.bombs)
       .set({
@@ -81,7 +88,6 @@ export async function submit(c: C) {
         time: new Date(),
       })
       .where(eq(schema.bombs.id, bombId));
-
     await db.insert(schema.explosions).values({
       bombId,
       phase,
